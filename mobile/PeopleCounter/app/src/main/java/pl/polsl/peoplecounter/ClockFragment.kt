3a0dd51a2e02028777f98b81c102b6f2abad1d8a
Gazda70 <pjgazda@interia.pl>
2021-11-30 22:30:17 +0100

    Calendar and clock added

---
+++ mobile/PeopleCounter/app/src/main/java/pl/polsl/peoplecounter/ClockFragment.kt
@@ -0,0 +1,31 @@
+package pl.polsl.peoplecounter
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ClockFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ClockFragment()
+    }
+
+    private lateinit var viewModel: ClockViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.clock_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ClockViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
